bc73ceffd8c46950de3fa02a7314b62a13424638
SGT SuPo <sgtsupo@gmail.com>
2025-04-30 20:34:12 +0700

    do exercise

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ android {
 }
 
 dependencies {
+    implementation("com.github.bumptech.glide:glide:4.15.0")
+    implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.squareup.picasso:picasso:2.8")
